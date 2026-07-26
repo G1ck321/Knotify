f21fc61fc91eebff5732cb33af345381090e9a48
G1ck321 <agbejimigbemiga@gmail.com>
2026-07-26 22:37:27 +0100

    feat: Added health endpoint to keep app online

--- Backend/routers/webhooks.py
+++ Backend/routers/webhooks.py
@@ -2,3 +2,17 @@ import httpx
 
 from fastapi.responses import JSONResponse
 from fastapi import APIRouter, HTTPException, status
+
+router = APIRouter(prefix="/webhook", tags=["Third Party Web Hooks"])
+
+@router.head("/health", status_code=status.HTTP_200_OK, tags=["System Health"])
+async def system_check():
+    """Light weight system function to check, and keep Render up"""
+
+    return JSONResponse(
+        content={
+            "status": "running",
+            "environment":"development",
+            "message": "KnotifyCU"
+        }
+    )
